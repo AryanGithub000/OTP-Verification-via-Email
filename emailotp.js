43f79b0639938ff24cc81cea09bd7036b218a14c
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-03-29 13:47:08 +0530

    Create emailotp.js

---
+++ emailotp.js
@@ -0,0 +1,36 @@
+function sendOTP() {
+	const email = document.getElementById('email');
+	const otpverify = document.getElementsByClassName('otpverify')[0];
+
+	let otp_val = Math.floor(Math.random() * 10000);
+
+	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
+	Email.send({
+	  SecureToken : "....", //get API token from SMTP after registering
+
+    To : email.value,
+	  From:"abc@gmail.com" //Enter your email id here
+    Subject : "Email OTP using JavaScript",
+    Body : emailbody,
+}).then(
+
+	message => {
+		if (message === "OK") {
+			alert("OTP sent to your email " + email.value);
+
+			otpverify.style.display = "flex";
+			const otp_inp = document.getElementById('otp_inp');
+			const otp_btn = document.getElementById('otp-btn');
+
+			otp_btn.addEventListener('click', () => {
+				if (otp_inp.value == otp_val) {
+					alert("Email address verified...");
+				}
+				else {
+					alert("Invalid OTP");
+				}
+			})
+		}
+	}
+);
+}
